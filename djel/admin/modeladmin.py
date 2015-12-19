c7f1441ffe6c7267cdf709b6322d5159d4fac5d6
Evgeniy Getmanskiy <getmansky@steelkiwi.com>
2015-12-19 19:12:46 +0200

    Django 1.9 support

--- djel/admin/modeladmin.py
+++ djel/admin/modeladmin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
+from django.contrib.admin.sites import AdminSite
 from django.core.urlresolvers import reverse
 from django.utils.encoding import force_unicode
 
@@ -14,7 +14,7 @@ def fk_link_method(fieldname, description):
             ajax_link = '<a href="{}?_popup=1" class="ajax-link"></a>'.format(url)
             return link + ajax_link
         else:
-            return EMPTY_CHANGELIST_VALUE
+            return self.empty_value_display
     f.allow_tags = True
     f.short_description = description
     f.admin_order_field = fieldname
