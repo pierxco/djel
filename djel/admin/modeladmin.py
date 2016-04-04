56154694ba23821b421d7564611124d6c2faf5be
Artem Antsyferov <antsyferov@steelkiwi.com>
2016-04-04 11:00:03 +0300

    added possibility to add custom query_param_key and changed filter-link from a to span to use javascript for filter/unfilter

--- djel/admin/modeladmin.py
+++ djel/admin/modeladmin.py
@@ -20,15 +20,20 @@ def get_empty_value_display(model_admin):
 
 
 def fk_link_method(fieldname, description, format_anchor_text_func=force_unicode, admin_order_field=True,
-                   add_filter=False):
+                   add_filter=False, query_param_key=None):
     def f(self, obj):
         field = getattr(obj, fieldname)
         if field is not None:
             url = reverse('admin:{}_{}_change'.format(field._meta.app_label, field._meta.model_name), args=(field.pk,))
             link = u'<b><a href="{}">{}</a></b>'.format(url, format_anchor_text_func(field))
             ajax_link = '<a href="{}?_popup=1" class="ajax-link"></a>'.format(url)
-            filter_link = u'<a href="?{}__id__exact={}" class="filter-link"></a>'.format(fieldname, field.pk)
             if add_filter:
+                if query_param_key:
+                    filter_link = u'<span data-param="{}={}" class="filter-link"></span>'.format(query_param_key,
+                                                                                                 field.pk)
+                else:
+                    filter_link = u'<span data-param="{}__id__exact={}" class="filter-link"></span>'.format(fieldname,
+                                                                                                            field.pk)
                 return link + '<div class="popup-filter">' + ajax_link + filter_link + '</div>'
             return link + ajax_link
         else:
